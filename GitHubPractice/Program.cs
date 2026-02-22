d087da7fae46c4f7ad599eb452a519882006ad8c
Amit Maske <mr.amitmaske@gmail.com>
2026-02-22 10:07:27 +0530

    add frequency programe

--- GitHubPractice/Program.cs
+++ GitHubPractice/Program.cs
@@ -1,16 +1,26 @@
 ﻿using System;
+using System.Linq;
 
 namespace practice { 
 
     class practice
     {
 
-        public void first() { 
-        
-        }
-
-        public static void Main(String[] args)
+       public static void Main(String[] args)
         {
+            var input = Console.ReadLine();
+
+            var output = input.GroupBy(c => c)
+                            .Select(g => new
+                            {
+                                character = g.Key,
+                                frequency = g.Count()
+                            });
+
+            foreach(var freq in output)
+            {
+                Console.WriteLine($"{freq.character}-{freq.frequency}");
+            }
 
         }
     }
