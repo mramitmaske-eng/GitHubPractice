ef8b8a8fb482adb81afec290224b8ee84b1a8651
Amit Maske <mr.amitmaske@gmail.com>
2026-02-22 13:17:43 +0530

    done

--- GitHubPractice/Program.cs
+++ GitHubPractice/Program.cs
@@ -1,27 +1,21 @@
 ﻿using System;
+using System.Globalization;
 using System.Linq;
 
-namespace practice { 
+namespace practice
+{
 
     class practice
     {
 
-       public static void Main(String[] args)
+        public static void Main(String[] args)
         {
-            var input = Console.ReadLine();
 
-            var output = input.GroupBy(c => c)
-                            .Select(g => new
-                            {
-                                character = g.Key,
-                                frequency = g.Count()
-                            });
 
-            foreach(var freq in output)
-            {
-                Console.WriteLine($"{freq.character}-{freq.frequency}");
-            }
 
         }
+
+
     }
 }
+
